87620d046f1645330374c8e9584ccf3d0a6306cf
Pedro Martinelli <p.martinellinunes@gmail.com>
2023-08-23 17:44:34 -0300

    criando DrawScreen e DrawRows para "desenhar" na tela

--- Menu.cs
+++ Menu.cs
@@ -1,6 +1,34 @@
-﻿namespace EditorHTML
+﻿namespace EditorHTML;
+
+public static class Menu
 {
-    internal class Menu
+    public static void Show()
     {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+
+        DrawScreen();
+    }
+
+    public static void DrawScreen()
+    {
+        DrawRows("+", "-", 30, 1, false, false);
+        DrawRows("|", " ", 30, 20, true, true);
+        DrawRows("+", "-", 30, 1, false, false);
+    }
+
+    public static void DrawRows(string limitCharacter, string colunmCharacter, int width, int height, Boolean newLineAbove, Boolean newLineUnder)
+    {
+        for (int x = 0; x < height; x++)
+        {
+            if (newLineAbove && x == 0) Console.WriteLine("");
+            Console.Write(limitCharacter);
+            for (int i = 0; i < width; i++) Console.Write(colunmCharacter);
+            Console.Write(limitCharacter);
+            if (newLineUnder) Console.WriteLine("");
+        }
+
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -4,8 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-
-
+        Menu.Show();
     }
 }
 
